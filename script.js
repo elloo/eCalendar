2b5505de397699300cec36700a6210fa5eb21e40
Ewe Lin Loo <ewelinloo@gmail.com>
2020-04-04 18:07:45 +1100

    Put DOM element selector into variable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+const calendarBody = document.getElementById('calendarDates')
+
 row = document.createElement('tr')
 cell = document.createElement('td')
 cellData = document.createTextNode('1')
 cell.appendChild(cellData)
 row.appendChild(cell)
-document.getElementById('calendarDates').appendChild(row)
+calendarBody.appendChild(row)
