c2e5f513916a0b00a0d83cbfbb8f638b46a94a01
Ewe Lin Loo <ewelinloo@gmail.com>
2020-04-04 19:07:08 +1100

    Add iterative for loops

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const calendarBody = document.getElementById('calendarDates')
+// Need number of days in month
+console.log(new Date(2020, 3, 0).getDate());
+// Create new row when 7 td have been created
+// Assign day num to day column
+console.log(new Date(2020, 3).getDay());
+// Array of getDay() and index notation
+// cellData[array[i]]
+// Fill previous month's days if needed
 
-row = document.createElement('tr')
-cell = document.createElement('td')
-cellData = document.createTextNode('1')
-cell.appendChild(cellData)
-row.appendChild(cell)
-calendarBody.appendChild(row)
+const calendarBody = document.getElementById('calendarDates');
+
+for (let i = 1; i <= 7; i++) {
+  row = document.createElement('tr');
+
+  for (let i = 1; i <= 7; i++) {
+    cell = document.createElement('td');
+    cellData = document.createTextNode(i);
+    cell.appendChild(cellData);
+    row.appendChild(cell);
+  }
+
+  calendarBody.appendChild(row);
+}
