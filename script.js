b3b3160131ba4d2b822a89eeb8921a06c6b363d1
Ewe Lin Loo <ewelinloo@gmail.com>
2020-04-04 19:27:44 +1100

    Add correct number of days

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 // Need number of days in month
-console.log(new Date(2020, 3, 0).getDate());
-// Create new row when 7 td have been created
+// console.log(new Date(2020, 3, 0).getDate());
+// DONE: Create new row when 7 td have been created
 // Assign day num to day column
-console.log(new Date(2020, 3).getDay());
+// console.log(new Date(2020, 3).getDay());
 // Array of getDay() and index notation
 // cellData[array[i]]
 // Fill previous month's days if needed
 
 const calendarBody = document.getElementById('calendarDates');
 
+const daysInMonth = new Date(2020, 4, 0).getDate();
+let calendarDays = 1;
+
 for (let i = 1; i <= 7; i++) {
+  if (calendarDays > daysInMonth) {
+    break;
+  }
+
   row = document.createElement('tr');
 
   for (let i = 1; i <= 7; i++) {
     cell = document.createElement('td');
-    cellData = document.createTextNode(i);
+    cellData = document.createTextNode(calendarDays);
     cell.appendChild(cellData);
     row.appendChild(cell);
+
+    if (calendarDays <= daysInMonth) {
+      calendarDays++;
+    } else {
+      break;
+    }
   }
 
   calendarBody.appendChild(row);
