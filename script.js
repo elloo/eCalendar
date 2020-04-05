170d036d6daef1d965fbe0e55dcd62dc0d9cf539
Ewe Lin Loo <ewelinloo@gmail.com>
2020-04-05 18:34:42 +1000

    Add comments

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 // DONE: Need number of days in month
-// console.log(new Date(2020, 4, 0).getDate());
 // DONE: Create new row when 7 td have been created
-// Assign day num to day column
-// console.log(new Date(2020, 3).getDay());
-// Array of getDay() and index notation
-// cellData[array[i]]
+// DONE: Assign day num to day column
 // Fill previous month's days if needed
 
 const calendarBody = document.getElementById('calendarDates');
@@ -19,15 +15,18 @@ for (let i = 2; i <= daysInMonth; i++) {
   calendarDays.push(i);
 }
 
+// Controlling number of table rows
 for (let i = 0; i <= 6; i++) {
-  if (dayCount > calendarDays.length) {
+  if (dayCount > daysInMonth) {
     break;
   }
 
   row = document.createElement('tr');
 
+  // Controlling number of table columns
   for (let i = 0; i <= 6; i++) {
     cell = document.createElement('td');
+    // Need -1 since dayCount starts at 1, not 0
     cellData = document.createTextNode(calendarDays[dayCount - 1]);
     cell.appendChild(cellData);
     row.appendChild(cell);
